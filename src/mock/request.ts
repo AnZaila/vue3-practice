f675185cebecc2b40231a8a1f01a4bedad454ce8
Anzai <anzai0304@163.com>
2026-09-05 18:53:44 +0800

    delete: 删除文件作为dist文件存放分支

--- src/mock/request.ts
+++
@@ -1,9 +0,0 @@
-function cloneData<T>(data: T): T {
-  return JSON.parse(JSON.stringify(data)) as T
-}
-
-export function mockRequest<T>(data: T, delay = 800): Promise<T> {
-  return new Promise((resolve) => {
-    window.setTimeout(() => resolve(cloneData(data)), delay)
-  })
-}
--- src/utils/bus.ts
+++
@@ -1,3 +0,0 @@
-// src/utils/bus.js
-import mitt from 'mitt'
-export const bus = mitt()
